8e10f1755b600fa47e1fe688f7251ed73db7755a
Oleg <oleg.krut.64@gmail.com>
2023-04-26 23:14:19 +0700

    routes (not done)

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,6 @@ Route::add('GET', '/hello', [Controller\Site::class, 'hello'])
 Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup']);
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
+Route::add('GET', '/subjects', [Controller\Site::class, 'subjects']);
+Route::add('GET', '/groups', [Controller\Site::class, 'groups']);
 Route::add(['GET'],'/sum', [Controller\Site::class, 'sum']); // TEST
