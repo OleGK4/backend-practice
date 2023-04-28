1b993107685d0ea6bab9745a2d2681933615d1d7
gr411_kaoev <oleg.krut.64@gmail.com>
2023-04-28 14:06:41 +0700

    group cards, loops to interact with

--- backend-practice.sql
+++ backend-practice.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Апр 26 2023 г., 10:12
--- Версия сервера: 8.0.30
--- Версия PHP: 8.1.9
+-- Хост: localhost
+-- Время создания: Апр 28 2023 г., 07:03
+-- Версия сервера: 10.3.28-MariaDB
+-- Версия PHP: 8.1.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,10 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `groups` (
-  `id` int NOT NULL COMMENT 'group id',
-  `name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'group name',
-  `course` int NOT NULL COMMENT 'group course number'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL COMMENT 'group id',
+  `name` varchar(150) NOT NULL COMMENT 'group name',
+  `course` int(11) NOT NULL COMMENT 'group course number'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `course`) VALUES
+(1, '411', 2),
+(2, '430', 1);
 
 -- --------------------------------------------------------
 
@@ -40,11 +48,18 @@ CREATE TABLE `groups` (
 --
 
 CREATE TABLE `group_subjects` (
-  `hours` int NOT NULL COMMENT 'Hours of subject for a group',
-  `subject_id` int NOT NULL COMMENT 'subject id',
-  `group_id` int NOT NULL COMMENT 'group id',
-  `semester` int NOT NULL COMMENT 'semester of studying the subject for specific group'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Subjects hours for group in choosen semester';
+  `hours` int(11) NOT NULL COMMENT 'Hours of subject for a group',
+  `subject_id` int(11) NOT NULL COMMENT 'subject id',
+  `group_id` int(11) NOT NULL COMMENT 'group id',
+  `semester` int(11) NOT NULL COMMENT 'semester of studying the subject for specific group'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Subjects hours for group in choosen semester';
+
+--
+-- Дамп данных таблицы `group_subjects`
+--
+
+INSERT INTO `group_subjects` (`hours`, `subject_id`, `group_id`, `semester`) VALUES
+(100, 1, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -53,14 +68,14 @@ CREATE TABLE `group_subjects` (
 --
 
 CREATE TABLE `marks` (
-  `id` int NOT NULL COMMENT 'mark id',
-  `marks` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'mark(s) of student for this lesson',
-  `student_id` int NOT NULL COMMENT 'student id',
-  `mark_control_id` int NOT NULL COMMENT 'id of mark control',
-  `comment` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'teachers comment for his mark',
-  `subject_id` int NOT NULL COMMENT 'subject id',
+  `id` int(11) NOT NULL COMMENT 'mark id',
+  `marks` varchar(150) NOT NULL COMMENT 'mark(s) of student for this lesson',
+  `student_id` int(11) NOT NULL COMMENT 'student id',
+  `mark_control_id` int(11) NOT NULL COMMENT 'id of mark control',
+  `comment` varchar(150) NOT NULL COMMENT 'teachers comment for his mark',
+  `subject_id` int(11) NOT NULL COMMENT 'subject id',
   `date` date NOT NULL COMMENT 'date of mark'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -69,9 +84,9 @@ CREATE TABLE `marks` (
 --
 
 CREATE TABLE `mark_control` (
-  `id` int NOT NULL COMMENT 'id of mark control type',
-  `name` int NOT NULL COMMENT 'name of mark control type'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Mark control types';
+  `id` int(11) NOT NULL COMMENT 'id of mark control type',
+  `name` int(11) NOT NULL COMMENT 'name of mark control type'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Mark control types';
 
 -- --------------------------------------------------------
 
@@ -80,15 +95,25 @@ CREATE TABLE `mark_control` (
 --
 
 CREATE TABLE `students` (
-  `id` int NOT NULL COMMENT 'student id',
-  `last_name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'student last name',
-  `first_name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'student first name',
-  `patronymic` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'student patronymic',
-  `gender` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'student gender',
+  `id` int(11) NOT NULL COMMENT 'student id',
+  `last_name` varchar(150) NOT NULL COMMENT 'student last name',
+  `first_name` varchar(150) NOT NULL COMMENT 'student first name',
+  `patronymic` varchar(150) NOT NULL COMMENT 'student patronymic',
+  `gender` varchar(150) NOT NULL COMMENT 'student gender',
   `date_of_birth` date NOT NULL COMMENT 'date of birth',
-  `address` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'address of student',
-  `group_id` int NOT NULL COMMENT 'id of the group'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Studens table';
+  `address` varchar(150) NOT NULL COMMENT 'address of student',
+  `group_id` int(11) NOT NULL COMMENT 'id of the group'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Studens table';
+
+--
+-- Дамп данных таблицы `students`
+--
+
+INSERT INTO `students` (`id`, `last_name`, `first_name`, `patronymic`, `gender`, `date_of_birth`, `address`, `group_id`) VALUES
+(1, 'qwew', 'qweqw', 'eqwewq', 'qwewqe', '2023-04-28', 'qwe', 1),
+(2, 'qwe', 'qweqw', 'qweqw', 'qweqw', '2023-04-20', 'qwe', 1),
+(3, 'qwe', 'qweqw', 'qweqw', 'qweqw', '2023-04-20', 'qwe', 2),
+(4, 'qwe', 'qweqw', 'qweqw', 'qweqw', '2023-04-20', 'qwe', 2);
 
 -- --------------------------------------------------------
 
@@ -97,9 +122,16 @@ CREATE TABLE `students` (
 --
 
 CREATE TABLE `subjects` (
-  `id` int NOT NULL COMMENT 'subject id',
-  `name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'subject name'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL COMMENT 'subject id',
+  `name` varchar(150) NOT NULL COMMENT 'subject name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`) VALUES
+(1, 'Physics');
 
 -- --------------------------------------------------------
 
@@ -108,9 +140,9 @@ CREATE TABLE `subjects` (
 --
 
 CREATE TABLE `subject_teachers` (
-  `subject_id` int NOT NULL COMMENT 'subject id',
-  `teacher_id` int NOT NULL COMMENT 'teacher id'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Table with subject teachers (many to many)';
+  `subject_id` int(11) NOT NULL COMMENT 'subject id',
+  `teacher_id` int(11) NOT NULL COMMENT 'teacher id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table with subject teachers (many to many)';
 
 -- --------------------------------------------------------
 
@@ -119,14 +151,14 @@ CREATE TABLE `subject_teachers` (
 --
 
 CREATE TABLE `teachers` (
-  `id` int NOT NULL COMMENT 'teacher id',
-  `last_name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'teacher last name',
-  `first_name` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'teacher first name',
-  `patronymic` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'teacher patronymic',
-  `gender` varchar(100) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'teacher gender',
+  `id` int(11) NOT NULL COMMENT 'teacher id',
+  `last_name` varchar(150) NOT NULL COMMENT 'teacher last name',
+  `first_name` varchar(150) NOT NULL COMMENT 'teacher first name',
+  `patronymic` varchar(150) NOT NULL COMMENT 'teacher patronymic',
+  `gender` varchar(100) NOT NULL COMMENT 'teacher gender',
   `date_of_birth` date NOT NULL COMMENT 'date of birth',
-  `address` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'address of teacher'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `address` varchar(150) NOT NULL COMMENT 'address of teacher'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -135,11 +167,20 @@ CREATE TABLE `teachers` (
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL COMMENT 'user id',
-  `login` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'user login',
-  `password` varchar(150) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'user password',
-  `type` tinyint(1) NOT NULL COMMENT 'admin or nor an admin'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Users table';
+  `id` int(11) NOT NULL COMMENT 'user id',
+  `login` varchar(150) NOT NULL COMMENT 'user login',
+  `password` varchar(150) NOT NULL COMMENT 'user password',
+  `type` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'admin or nor an admin',
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Users table';
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `type`, `name`) VALUES
+(1, 'fatguy', '76d80224611fc919a5d54f0ff9fba446', 0, 'fatty'),
+(2, 'login', '1a1dc91c907325c69271ddf0c944bc72', 0, 'name');
 
 --
 -- Индексы сохранённых таблиц
@@ -152,10 +193,20 @@ ALTER TABLE `groups`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `group_subjects`
+--
+ALTER TABLE `group_subjects`
+  ADD KEY `group_id` (`group_id`),
+  ADD KEY `group_subjects_ibfk_2` (`subject_id`);
+
+--
 -- Индексы таблицы `marks`
 --
 ALTER TABLE `marks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `mark_control_id` (`mark_control_id`);
 
 --
 -- Индексы таблицы `mark_control`
@@ -167,7 +218,8 @@ ALTER TABLE `mark_control`
 -- Индексы таблицы `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `group_id` (`group_id`);
 
 --
 -- Индексы таблицы `subjects`
@@ -195,43 +247,68 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'group id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'group id', AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `marks`
 --
 ALTER TABLE `marks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'mark id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'mark id';
 
 --
 -- AUTO_INCREMENT для таблицы `mark_control`
 --
 ALTER TABLE `mark_control`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'id of mark control type';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id of mark control type';
 
 --
 -- AUTO_INCREMENT для таблицы `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'student id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'student id', AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'subject id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'subject id', AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'teacher id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'teacher id';
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'user id';
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user id', AUTO_INCREMENT=3;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `group_subjects`
+--
+ALTER TABLE `group_subjects`
+  ADD CONSTRAINT `group_subjects_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `group_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `marks`
+--
+ALTER TABLE `marks`
+  ADD CONSTRAINT `marks_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `marks_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `marks_ibfk_3` FOREIGN KEY (`mark_control_id`) REFERENCES `mark_control` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
