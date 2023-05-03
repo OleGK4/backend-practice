003ada6a305a8b09dcea97b54c94f6b04489314c
Oleg <oleg.krut.64@gmail.com>
2023-05-03 21:24:41 +0700

    file utility;

--- views/site/notifications/success_add.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <title>Notification</title>
-</head>
-<div class="d-flex justify-content-center">
-    <div class="form-container" style="height: max-content; padding: 20px">
-        <h2 class="block-header-h2">Успешное добавление!</h2>
-    </div>
-</div>
