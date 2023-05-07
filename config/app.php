4e87392db3eb86476ac9f8ac8e49f008c99ee5d7
Oleg <oleg.krut.64@gmail.com>
2023-05-07 12:56:40 +0700

    api guide done;

--- config/app.php
+++ config/app.php
@@ -16,9 +16,18 @@ return [
         'auth' => \Middlewares\AuthMiddleware::class,
         'admin' => \Middlewares\AdminMiddleware::class,
     ],
+    //Классы для middleware
     'routeAppMiddleware' => [
         'csrf' => \Middlewares\CSRFMiddleware::class,
         'trim' => \Middlewares\TrimMiddleware::class,
         'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
+    ],
+    //Классы провайдеров
+    'providers' => [
+        'kernel' => \Providers\KernelProvider::class,
+        'route' => \Providers\RouteProvider::class,
+        'db' => \Providers\DBProvider::class,
+        'auth' => \Providers\AuthProvider::class,
     ],
 ];
