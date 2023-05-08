f7da3931b6d47607b4f7e789f7dbbf12695091fd
Oleg <oleg.krut.64@gmail.com>
2023-05-08 12:22:58 +0700

    api v2;

--- app/Controller/Api.php
+++ app/Controller/Api.php
@@ -2,6 +2,7 @@
 
 namespace Controller;
 
+use Model\Group_subject;
 use Model\Student;
 use Src\Auth\Auth;
 use Src\Request;
@@ -9,11 +10,18 @@ use Src\View;
 
 class Api
 {
-    public function index(): void
+    public function students(): void
     {
-        $posts = Student::all()->toArray();
 
-        (new View())->toJSON($posts);
+        $students = Student::all()->toArray();
+
+        (new View())->toJSON($students);
+    }
+
+    public function groupSubjects(): void
+    {
+        $groupSubjects = Group_subject::all()->toArray();
+        (new View())->toJSON($groupSubjects);
     }
 
     public function echo(Request $request): void
@@ -35,5 +43,17 @@ class Api
                 (new View())->toJSON((array)'Login failed');
             }
         }
+    }
+
+    public function logout(Request $request)
+    {
+        if (!Auth::attempt($request->all())) {
+            $token = null;
+            Auth::user()->update([
+                'token' => $token
+            ]);
+        }
+        Auth::logout();
+        (new View())->toJSON((array)'logout');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Src\Route;
 
-Route::add('GET', '/', [Controller\Api::class, 'index']);
+Route::add('GET', '/students', [Controller\Api::class, 'students']);
+Route::add('GET', '/group_subjects', [Controller\Api::class, 'groupSubjects']);
+
+
 Route::add('POST', '/echo', [Controller\Api::class, 'echo']);
 Route::add('POST', '/login', [Controller\Api::class, 'login']);
+Route::add('GET', '/logout', [Controller\Api::class, 'logout']);
